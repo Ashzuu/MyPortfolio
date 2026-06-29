964d212182f21f72087567d83a42836a5cb5496e
Ashzuu <116486394+Ashzuu@users.noreply.github.com>
2026-06-30 00:17:54 +0200

    Feat/improve ux (#3)
    * fix: Changing dimensions, size of all components, but effect in the cursor missing
    
    * feat/effect in the cursor applied
    
    * feat/effect: Title apparition effect
    
    ---------
    
    Co-authored-by: Ashzuu <evan.dynak@bbox.fr>

--- app/components/FirstSectionComponent.vue
+++ app/components/FirstSectionComponent.vue
@@ -1,7 +1,68 @@
 <script setup lang="ts">
-
 import Badge from "~/components/usable/Badge.vue";
 import Button from "~/components/usable/Button.vue";
+
+
+/**
+ * Function to launch the animation with all letters show quickly letter by letter
+ * @param textToReach The text to reach after the animation
+ * @param element The element to change
+ * @param startDelay Delay in ms before starting the animation
+ */
+function rollAndPickAnimation(textToReach: string, element: HTMLElement, startDelay: number = 0) {
+  const chars: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  let currentLength = 0;
+  let randomFrames = 0;
+  const maxRandomFrames = 15;
+
+  element.innerText = "";
+
+  setTimeout(() => {
+    const interval = setInterval(() => {
+      if (currentLength >= textToReach.length) {
+        element.innerText = textToReach;
+        clearInterval(interval);
+        return;
+      }
+
+      const revealedText = textToReach.substring(0, currentLength);
+      let randomChar = "";
+
+      if (textToReach[currentLength] !== ' ') {
+        randomChar = chars.charAt(Math.floor(Math.random() * chars.length));
+      } else {
+        randomChar = " ";
+        randomFrames = maxRandomFrames;
+      }
+
+      element.innerText = revealedText + randomChar;
+
+      randomFrames++;
+      if (randomFrames > maxRandomFrames) {
+        randomFrames = 0;
+        currentLength++;
+      }
+    }, 5);
+  }, startDelay);
+}
+
+function startAnimationTitleAndShowing(){
+  const titlePart = document.getElementById("title-first-part");
+  const highlightedTitle = document.getElementById("title-highlighted");
+  if (titlePart && highlightedTitle) {
+    rollAndPickAnimation("Découvrez mon", titlePart, 0);
+    rollAndPickAnimation("Portfolio", highlightedTitle, 800);
+  }
+  const firstPart = document.getElementById("first-section-container");
+  if(firstPart){
+    firstPart.style.opacity = "1";
+  }
+}
+
+if(import.meta.client){
+  window.onload = startAnimationTitleAndShowing;
+}
+
 </script>
 
 <template>
@@ -10,7 +71,7 @@ import Button from "~/components/usable/Button.vue";
       <div id="first-section-badge-container">
         <Badge class="primary-color-override">Musique</Badge><Badge class="secondary-color-override">Informatique</Badge>
       </div>
-      <h1>Découvrez mon<br><span class="synth-wave-color large">Portfolio</span></h1>
+      <h1 id="main-title"><span id="title-first-part"></span><br><span id="title-highlighted" class="synth-wave-color large"></span></h1>
       <p class="description-content">Ce site a pour but de vous montrer mes différentes réalisations informatiques que j'ai pu réaliser tout au long de mon parcours,
       mais aussi parler un peu de moi et mes passions</p>
       <div id="buttons-section-container">
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import FirstSectionComponent from "~/components/FirstSectionComponent.vue";
-import ProjectSectionComponent from "~/components/ProjectSectionComponent.vue";
-import AboutMeSectionComponent from "~/components/AboutMeSectionComponent.vue";
-import ContactSectionComponent from "~/components/ContactSectionComponent.vue";
+  import FirstSectionComponent from "~/components/FirstSectionComponent.vue";
+  import ProjectSectionComponent from "~/components/ProjectSectionComponent.vue";
+  import AboutMeSectionComponent from "~/components/AboutMeSectionComponent.vue";
+  import ContactSectionComponent from "~/components/ContactSectionComponent.vue";
 </script>
 <template>
-  <div>
+  <div id="main-component">
+    <div id="mouse-decoration">
+      <div class="little-effect"/>
+    </div>
     <NuxtRouteAnnouncer />
     <FirstSectionComponent />
     <ProjectSectionComponent />
@@ -14,4 +17,40 @@ import ContactSectionComponent from "~/components/ContactSectionComponent.vue";
   </div>
 </template>
 <style>
+  #main-component {
+    display: flex;
+    flex-direction: column;
+    gap: 10dvh;
+    cursor: none;
+  }
+
+  #mouse-decoration {
+    z-index: 100;
+    pointer-events: none;
+    position: absolute;
+    visibility: hidden;
+    aspect-ratio: 1/1;
+    border-radius: 100%;
+    background-color: rgb(170 0 255 / 0.8);
+
+
+    .little-effect {
+      opacity: 1;
+      position: relative;
+      width: 100%;
+      height: 100%;
+      border-radius: 100%;
+      box-shadow: 0 0 20px 20px rgb(247 0 255 / 0.3);
+      animation: haloAnimation 2.2s ease-in-out infinite alternate;
+    }
+  }
+
+  @keyframes haloAnimation {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0.4;
+    }
+  }
 </style>
