9fb7282368ade6440fbf32ba840f79fd8365134f
Ashzuu <evan.dynak@bbox.fr>
2026-06-29 21:58:21 +0200

    fix: Changing dimensions, size of all components, but effect in the cursor missing

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-import FirstSectionComponent from "~/components/FirstSectionComponent.vue";
-import ProjectSectionComponent from "~/components/ProjectSectionComponent.vue";
-import AboutMeSectionComponent from "~/components/AboutMeSectionComponent.vue";
-import ContactSectionComponent from "~/components/ContactSectionComponent.vue";
+  import FirstSectionComponent from "~/components/FirstSectionComponent.vue";
+  import ProjectSectionComponent from "~/components/ProjectSectionComponent.vue";
+  import AboutMeSectionComponent from "~/components/AboutMeSectionComponent.vue";
+  import ContactSectionComponent from "~/components/ContactSectionComponent.vue";
+
+  if(import.meta.browser){
+    const onMouseMove = (e: MouseEvent) => {
+
+    }
+  }
 </script>
 <template>
-  <div>
+  <div id="main-component">
+    <div id="mouse-decoration" @mousemove="onMouseMove"/>
     <NuxtRouteAnnouncer />
     <FirstSectionComponent />
     <ProjectSectionComponent />
@@ -14,4 +21,17 @@ import ContactSectionComponent from "~/components/ContactSectionComponent.vue";
   </div>
 </template>
 <style>
+  #main-component {
+    display: flex;
+    flex-direction: column;
+    gap: 10dvh;
+  }
+
+  #mouse-decoration {
+    position: absolute;
+    width: 20px;
+    aspect-ratio: 1/1;
+    border-radius: 20px;
+    background-color: rgb(255 255 255 / 0.5);
+  }
 </style>
