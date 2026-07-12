083d9c03a8fc4704a71ca0166a80e76c1440a51d
Ashzuu <evan.dynak@bbox.fr>
2026-07-12 20:16:06 +0200

    fix: bug of the pointer event when scrolling

--- app/app.vue
+++ app/app.vue
@@ -15,9 +15,9 @@ function isMobile() {
 
 const handleMouseMove = (event: MouseEvent) => {
   if (cursorRef.value) {
-    cursorRef.value.style.visibility = VISIBILITY
-    cursorRef.value.style.top = event.pageY - SIZE_CURSOR / 2 + 'px'
-    cursorRef.value.style.left = event.pageX - SIZE_CURSOR / 2 + 'px'
+    cursorRef.value.style.visibility = VISIBILITY;
+    cursorRef.value.style.top = event.clientY - SIZE_CURSOR / 2 + 'px';
+    cursorRef.value.style.left = event.clientX - SIZE_CURSOR / 2 + 'px';
   }
 }
 
@@ -51,9 +51,9 @@ const handleMouseOver = (event: MouseEvent) => {
 
 onMounted(() => {
   if (cursorRef.value && !isMobile()) {
-    cursorRef.value.style.width = SIZE_CURSOR + "px"
-    window.addEventListener('mousemove', handleMouseMove)
-    window.addEventListener('mouseover', handleMouseOver)
+    cursorRef.value.style.width = SIZE_CURSOR + "px";
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseover', handleMouseOver);
   }
 })
 
@@ -78,7 +78,7 @@ onUnmounted(() => {
   #mouse-decoration {
     z-index: 100;
     pointer-events: none;
-    position: absolute;
+    position: fixed;
     visibility: hidden;
     aspect-ratio: 1/1;
     border-radius: 100%;
---
+++ app/pages/projects/copupitre.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { ProjectService } from "~/services/ProjectService";
+  import type {Project} from "~/models/Project";
+
+  const service: ProjectService = new ProjectService();
+  const project: Project = service.specificProject(7)!;
+</script>
+
+<template>
+  <ProjectsBaseComponent :project="project">
+
+  </ProjectsBaseComponent>
+</template>
+
+<style scoped>
+
+</style>
